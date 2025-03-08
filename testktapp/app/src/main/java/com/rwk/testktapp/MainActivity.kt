b0dd8055efab68e25cb744387fe93f1cd8acedd3
Antonio Ramar Collins II <ramar@ramar.work>
2025-03-07 20:20:30 -0500

    Using one of the Compose MP projects as a base, I was able to get most of these dependencies updated.   Need to change a few strings for the test to compile, add a theme, and finally turn each of these files back into a template so we can build compliant projects easily and quickly.

---
+++ testktapp/app/src/main/java/com/rwk/testktapp/MainActivity.kt
@@ -0,0 +1,50 @@
+// MainActivity.java - Write your own stuff in here to get started...
+package com.rwk.testktapp; 
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+// Import themes with this.  Never been so simple
+import com.example.kotlintestapp.ui.theme.KotlinTestAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            //KotlinTestAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            //}
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KotlinTestAppTheme {
+        Greeting("Android")
+    }
+}
