2499bda2c8b00cedb85d28e08b633c4409f3f9ee
Antonio Ramar Collins II <ramar@ramar.work>
2025-03-09 14:13:27 -0400

    Made significant changes to configure, Makefile and environment templates
    - Many tiny changes to PATHs and library prefixes
    - To make everything simple, everything is now under one prefix.

---
+++ java.mk
@@ -0,0 +1,6 @@
+# TODO: Include Java build info here
+#JDKBIN = OpenJDK8U-jdk_x64_$(BRANCH)_hotspot_8u312b07
+#JDKVER = jdk8u312-b07
+JDKBIN = OpenJDK17U-jdk_x64_$(BRANCH)_hotspot_17.0.14_7
+JDKVER = jdk-17.0.14+7
+
